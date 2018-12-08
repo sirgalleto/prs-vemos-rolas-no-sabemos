e9b64cbd5f232004f031515ccd19fbcddcd13d93
sirgalleto <sebastian.osorio@wizeline.com>
2018-12-07 19:31:14 -0600

    Add flow to get songs between two commits

--- src/lib/git.ts
+++ src/lib/git.ts
@@ -1,4 +1,6 @@
-import { Repository, Note } from "nodegit";
+import { Repository, Note, Oid, Commit } from "nodegit";
+
+const notesRef = "refs/notes/commits";
 
 export default class Git {
   static async createNoteForLastCommit(note: string) {
@@ -8,11 +10,11 @@ export default class Git {
       const commit = await repository.getBranchCommit(currentBranch.name());
       const author = commit.author();
       const oid = commit.id();
-      const trackMessage = note
+      const trackMessage = note;
 
       await Note.create(
         repository,
-        "refs/notes/commits",
+        notesRef,
         author,
         author,
         oid,
@@ -21,7 +23,61 @@ export default class Git {
       );
     } catch (error) {
       console.error("There was an error adding the music note:", error);
-      throw new Error(error)
+      throw new Error(error);
     }
+  }
+
+  static async getCommitsBetweenTwoCommits(
+    startCommit: string,
+    endCommit: string
+  ) {
+    return new Promise<any[]>(async (resolve, reject) => {
+      try {
+        const repository = await Repository.open("./");
+
+        const startCommitOid = Oid.fromString(startCommit);
+        const startCommitObject = await Commit.lookup(
+          repository,
+          startCommitOid
+        );
+
+        const eventEmitter = startCommitObject.history();
+
+        eventEmitter.on("end", function(commits) {
+          const filteredCommits = [startCommitObject];
+          let index = 0;
+
+          while (commits[index].id().tostrS() !== endCommit) {
+            filteredCommits.push(commits[index]);
+            index++;
+          }
+
+          resolve(filteredCommits);
+        });
+
+        eventEmitter.start();
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+
+  static async getSongsBetweenTwoCommits(
+    startCommit: string,
+    endCommit: string
+  ) {
+    const repository = await Repository.open("./");
+    const commits: any[] = await Git.getCommitsBetweenTwoCommits(
+      startCommit,
+      endCommit
+    );
+
+    const notes = await Promise.all(
+      commits.map(commit => {
+        return Note.read(repository, notesRef, commit.id());
+      })
+    );
+
+    return notes.map(note => note.message())
   }
 }
