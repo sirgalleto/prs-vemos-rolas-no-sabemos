724e9967c73ba42d0aab2fabd1a7c70590e62d5c
sirgalleto <sebastian.osorio@wizeline.com>
2018-12-07 18:20:09 -0600

    Update name to set credentials

---
+++ src/commands/set-credentials.ts
@@ -0,0 +1,24 @@
+import { Command } from "@oclif/command";
+import cli from "cli-ux";
+import Client from '../lib/credentials'
+
+export default class Hello extends Command {
+  static description =
+    "adds spotify application credentials";
+
+  static examples = [`$ pvrns login`];
+
+  static flags = {};
+  static args = [];
+
+  async run() {
+    try {
+      const clientId: string = await cli.prompt('clientId')
+      const clientSecret: string = await cli.prompt('clientSecret')
+
+      Client.set({ clientId, clientSecret })
+    } catch(error) {
+		  console.error("​It was an error saving your credentials")
+    }
+  }
+}
